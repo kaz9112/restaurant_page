08ee38f3f0cc13702b9e5e1d6c6217ecb2165e51
kaz9112 <samuel.k.naiborhu@gmail.com>
2024-09-24 21:07:44 +0800

    added dom to add h1 on home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./styles.css";
+
+
+const MainPages = (function() {
+    
+    const main = document.querySelector('main');
+
+
+    const Home = (function() {
+        main.innerHtml = '';
+        function createChild () {
+            const h1 = document.createElement('h1');
+            h1.textContent = 'Home';
+
+            const getHeader = () => h1;
+
+            return h1
+        }
+
+        const homeChild = createChild()
+        main.appendChild(homeChild)
+
+        // console.log('fck')
+    });
+
+    Home()
+})();
