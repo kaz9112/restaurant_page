75c68fbf9a1b178fff70d439f4dee013b78cffd7
kaz9112 <samuel.k.naiborhu@gmail.com>
2024-09-26 20:57:22 +0800

    added funcgtion for changing pages for home and menu

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ const MainPages = (function() {
     
     const main = document.querySelector('main');
 
-
     const Home = (function() {
-        main.innerHtml = '';
+        main.innerHTML = '';
         function createChild () {
             const h1 = document.createElement('h1');
             h1.textContent = 'Home';
@@ -25,18 +24,63 @@ const MainPages = (function() {
             const divOneP2 = document.createElement('p');
             divOneP2.textContent = 'fu off'
 
-            divOne.appendChild(divOneP1)
-            divOne.appendChild(divOneP2)
+            divOne.appendChild(h3);
+            divOne.appendChild(divOneP1);
+            divOne.appendChild(divOneP2);
 
 
-            return h1
+            return { h1, divOne };
         }
 
-        const homeChild = createChild()
-        main.appendChild(homeChild)
+        const { h1, divOne }= createChild();
+
+        // const homeChild = createChild()
+        main.appendChild(h1);
+        main.appendChild(divOne);
 
         // console.log('fck')
     });
 
+    const Menu = (function() {
+        main.innerHTML = '';
+        function createChild () {
+            const h1 = document.createElement('h1');
+            h1.textContent = 'Menu';
+
+            const divOne = document.createElement('div');
+
+            const h3 = document.createElement('h3');
+            h3.textContent = 'fuker off';
+
+            const divOneP1 = document.createElement('p');
+            divOneP1.textContent = 'criesommure';
+
+            divOne.appendChild(h3);
+            divOne.appendChild(divOneP1);
+
+            return { h1, divOne };
+        }
+
+        const { h1, divOne } = createChild();
+
+        main.appendChild(h1);
+        main.appendChild(divOne);
+    });
+
     Home()
+
+    document.getElementById('home').addEventListener("click", () => {
+        // console.log("home")
+        Home()
+    })
+
+    document.getElementById('menu').addEventListener("click", () => {
+        // console.log("menu")
+        Menu()
+    })
+
+    document.getElementById('about').addEventListener("click", () => {
+        console.log("about")
+    })
+
 })();
